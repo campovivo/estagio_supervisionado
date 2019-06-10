7cd1cf1a040462e6347fb8c4653c0343e9a4fc1f
dyegoabrantes <dyegoabrantes@gmail.com>
2019-06-10 12:35:22 -0300

    Correções estéticas

--- static/scripts.js
+++ static/scripts.js
@@ -1,20 +1,21 @@
 const mySiema = new Siema({
-    loop: true,
+	loop: true,
+	easing: 'cubic-bezier(0.945, 0.345, 0.655, 1)'
 });
 
 setInterval(() => mySiema.next(), 4000);
 
 new fullpage('#fullpage', {
-    scrollBar: true,
+    // scrollBar: true,
     onLeave: function(origin, destination, direction){
 		var leavingSection = this;
 
-		if(origin.index == 0 && direction =='down'){
-			console.log("Going to section 2!");
-		}
+		// if(origin.index == 0 && direction =='down'){
+		// 	console.log("Going to section 2!");
+		// }
 
-		else if(origin.index == 2 && direction == 'up'){
-			console.log("Going to section 2!");
-		}
+		// else if(origin.index == 2 && direction == 'up'){
+		// 	console.log("Going to section 2!");
+		// }
 	}
 });
